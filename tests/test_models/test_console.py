57d233a7317259b86625dd3c6afabf4b74a044b4
NikkyXO <alexisdame2017@gmail.com>
2022-08-04 19:11:25 +0100

    added tests

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -38,7 +38,7 @@ class TestHBNBCommand(unittest.TestCase):
         """
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['tests/test_models/test_console.py'])
-        self.assertEqual(result.total_errors, 1,
+        self.assertEqual(result.total_errors, 0,
                          "Found code style errors (and warnings).")
 
     def test_module_docstring(self):
